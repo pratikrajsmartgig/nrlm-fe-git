f543242b30ebea77355f84e07d319ba2c9961960
Pratik Raj <praj299@gmail.com>
2023-12-21 16:00:56 +0530

    updated code

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    url: 'http://70.35.200.27:8081/api/',
+    // url: 'http://70.35.200.27:8081/api/',
+   url: 'http://74.208.117.118:8081/api/',
 };
