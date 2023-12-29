fde91703c8e04e03cd65d94aa9073c60c4c36fe8
Pratik Raj <praj299@gmail.com>
2023-12-29 11:52:00 +0530

    environment update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-   // url: 'http://70.35.200.27:8081/api/',
-    url: 'http://74.208.117.118:8081/api/',
+  // url: 'http://70.35.200.27:8081/api/',
+    // url: 'http://74.208.117.118:8081/api/',
+    url: 'https://sit2nrlm5api.finakon.in/api/'
 };
